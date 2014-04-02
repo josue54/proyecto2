4e6245468322d8b1db881a59b8de29392263f048
josue54 <jraabarca@gmail.com>
2014-04-02 12:11:51 -0600

    cambiado orden de carpetas

--- proyecto2/application/config/config.php
+++ proyecto2/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/proyecto2/codeigniter';
+$config['base_url']	= 'http://localhost/proyecto2/';
 
 /*
 |--------------------------------------------------------------------------
